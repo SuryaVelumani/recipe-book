c30d7d6db35a10836c73b01416d7000c46ce1b4e
Surya V <76275102+SuryaVelumani@users.noreply.github.com>
2021-12-11 12:18:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import NavBar from './components/NavBar'
-
+import React from 'react';
+import Profilemain from './Profilemain';
+import Cusines from './Cusines'
+import { Route, BrowserRouter as Router } from "react-router-dom"
 function App() {
   return (
-    <div className='App'>
-      <NavBar />
-    </div>
+    <Router>
+      <div className='app'>
+      </div>
+      <Route path="/profile" component={Profilemain} />
+
+      <Route path="/cusines" component={Cusines} />
+    </Router>
   )
 }
 
